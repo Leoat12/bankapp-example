ca2a05cc68fecc385733d7dc21bfe66d5bff0485
Leonardo <leoat12@gmail.com>
2018-11-25 12:35:40 -0200

    Jib & Sentry configuration.

--- src/main/java/com/leoat12/example/bankapp/BankappApplication.java
+++ src/main/java/com/leoat12/example/bankapp/BankappApplication.java
@@ -1,10 +1,16 @@
 package com.leoat12.example.bankapp;
 
+import com.leoat12.example.bankapp.exception.ResourceNotFoundException;
+import io.sentry.spring.SentryExceptionResolver;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.ServletContextInitializer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.servlet.HandlerExceptionResolver;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 @SpringBootApplication
 public class BankappApplication {
@@ -15,7 +21,20 @@ public class BankappApplication {
 
     @Bean
     public HandlerExceptionResolver sentryExceptionResolver() {
-        return new io.sentry.spring.SentryExceptionResolver();
+        return new SentryExceptionResolver() {
+            @Override
+            public ModelAndView resolveException(HttpServletRequest request,
+                                                 HttpServletResponse response,
+                                                 Object handler,
+                                                 Exception ex) {
+
+                if(!(ex instanceof ResourceNotFoundException))
+                    super.resolveException(request, response, handler, ex);
+
+                return null;
+
+            }
+        };
     }
 
     @Bean
